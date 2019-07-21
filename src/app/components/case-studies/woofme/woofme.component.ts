a5c6b3733f808b972f9e2ff86d6d1d8a4a9daf3c
paugmnoz <paulaagarcia1@gmail.com>
2019-07-21 13:38:11 -0500

    imgs routes changed

--- src/app/components/case-studies/woofme/woofme.component.ts
+++ src/app/components/case-studies/woofme/woofme.component.ts
@@ -13,20 +13,20 @@ export class WoofmeComponent implements OnInit {
   }
  
   slides = [
-    {img: "../../../../assets/projects/woof/app/1.png", ti:"INITIAL SCREEN", te:"Welcome screen"},
-    {img: "../../../../assets/projects/woof/app/2.png", ti:"GENERAL HOME SCREEN", te:"Create a dog profile"},
-    {img: "../../../../assets/projects/woof/app/3.png", ti:"GENERAL HOME SCREEN", te:"Create a dog profile"},
-    {img: "../../../../assets/projects/woof/app/4.png", ti:"UNSET PROFILE HOME SCREEN", te:"Dog owners will be asked to set a goal for the training according to their schedule."},
-    {img: "../../../../assets/projects/woof/app/5.png", ti:"PROFILE HOME SCREEN", te:"Feedback about goal status, and call to action in case today’s isn’t reach yet."},
-    {img: "../../../../assets/projects/woof/app/7.png", ti:"TRAINING SESSION SCREEN ", te:"During training sessions, dogs owners will be asked to record the excercises shown in the video guide."},
-    {img: "../../../../assets/projects/woof/app/6.png", ti:"DOG BEHAVIOR STATS SCREEN", te:"With the data recorded during sessions and the inclinometer of the vest, owners will be capable of see data stats about their dog’s behavior through time."}
+    {img: "./assets/projects/woof/app/1.png", ti:"INITIAL SCREEN", te:"Welcome screen"},
+    {img: "./assets/projects/woof/app/2.png", ti:"GENERAL HOME SCREEN", te:"Create a dog profile"},
+    {img: "./assets/projects/woof/app/3.png", ti:"GENERAL HOME SCREEN", te:"Create a dog profile"},
+    {img: "./assets/projects/woof/app/4.png", ti:"UNSET PROFILE HOME SCREEN", te:"Dog owners will be asked to set a goal for the training according to their schedule."},
+    {img: "./assets/projects/woof/app/5.png", ti:"PROFILE HOME SCREEN", te:"Feedback about goal status, and call to action in case today’s isn’t reach yet."},
+    {img: "./assets/projects/woof/app/7.png", ti:"TRAINING SESSION SCREEN ", te:"During training sessions, dogs owners will be asked to record the excercises shown in the video guide."},
+    {img: "./assets/projects/woof/app/6.png", ti:"DOG BEHAVIOR STATS SCREEN", te:"With the data recorded during sessions and the inclinometer of the vest, owners will be capable of see data stats about their dog’s behavior through time."}
   ];
 
   slideConfig = {
     'centerMode': true,
     "slidesToShow": 3, 
-    "nextArrow":"<div class='nav-btn next-slide' style='cursor:pointer'><img class='' src='../../../../../assets/nav-icon-right.svg' width='30px' alt=''></div>",
-    "prevArrow":"<div class='nav-btn prev-slide' style='cursor:pointer'><img class='' src='../../../../../assets/nav-icon-left.svg' width='30px' alt=''></div>",
+    "nextArrow":"<div class='nav-btn next-slide' style='cursor:pointer'><img class='' src='./assets/nav-icon-right.svg' width='30px' alt=''></div>",
+    "prevArrow":"<div class='nav-btn prev-slide' style='cursor:pointer'><img class='' src='./assets/nav-icon-left.svg' width='30px' alt=''></div>",
     "dots":true,
     "infinite": false,
     'responsive': [ 
