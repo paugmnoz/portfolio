fee96274747e7c9aaa8196f1d33592416ce5a6b4
Paula <paula.garcia1@correo.icesi.edu.co>
2020-04-10 18:22:22 -0500

    passions styles finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,8 @@ import Parallax from 'parallax-js';
 })
 export class HomeComponent implements OnInit {
   @ViewChild('scene') sceneElement:ElementRef;
+  @ViewChild('scene2') sceneElement2:ElementRef;
+  @ViewChild('scene3') sceneElement3:ElementRef;
   
   ngOnInit() {
   }
@@ -17,12 +19,22 @@ export class HomeComponent implements OnInit {
   scene: any;
   parallaxInstance: any;
   sceneDiv: any;
+  sceneDiv2: any;
+  sceneDiv3: any;
 
   ngAfterViewInit() {
     console.log(this.sceneElement.nativeElement);
     this.sceneDiv = this.sceneElement.nativeElement;
+    this.sceneDiv2 = this.sceneElement2.nativeElement;
+    this.sceneDiv3 = this.sceneElement3.nativeElement;
+
     var scene = this.sceneDiv;
+    var scene2 = this.sceneDiv2;
+    var scene3 = this.sceneDiv3;
+    
     var parallaxInstance = new Parallax(scene);
+    var parallaxInstance2 = new Parallax(scene2);
+    var parallaxInstance3 = new Parallax(scene3);
   }
   constructor() {
      // for more details on config options please visit fullPage.js docs
