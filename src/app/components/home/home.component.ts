23a699ba3080189789c25d2bd64ab928d8a79c04
Paula <paula.garcia1@correo.icesi.edu.co>
2020-05-03 20:17:11 -0500

    feature works start

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,7 @@ import Parallax from 'parallax-js';
 })
 export class HomeComponent implements OnInit {
   @ViewChild('scene') sceneElement:ElementRef;
+  @ViewChild('mob_scene') mobSceneElement:ElementRef;
   @ViewChild('scene2') sceneElement2:ElementRef;
   @ViewChild('scene3') sceneElement3:ElementRef;
   
@@ -17,22 +18,25 @@ export class HomeComponent implements OnInit {
   config: any;
   fullpage_api: any;
   scene: any;
-  parallaxInstance: any;
   sceneDiv: any;
+  mobSceneDiv: any;
   sceneDiv2: any;
   sceneDiv3: any;
 
   ngAfterViewInit() {
     console.log(this.sceneElement.nativeElement);
     this.sceneDiv = this.sceneElement.nativeElement;
+    this.mobSceneDiv = this.sceneElement.nativeElement;
     this.sceneDiv2 = this.sceneElement2.nativeElement;
     this.sceneDiv3 = this.sceneElement3.nativeElement;
 
     var scene = this.sceneDiv;
+    var mobScene = this.mobSceneDiv;
     var scene2 = this.sceneDiv2;
     var scene3 = this.sceneDiv3;
     
     var parallaxInstance = new Parallax(scene);
+    var mobParallaxInstance = new Parallax(mobScene);
     var parallaxInstance2 = new Parallax(scene2);
     var parallaxInstance3 = new Parallax(scene3);
   }
@@ -42,7 +46,7 @@ export class HomeComponent implements OnInit {
 
       // fullpage options
       licenseKey: 'YOUR LICENSE KEY HERE',
-      anchors: ['', 'love-to', 'thirdPage', 'fourthPage', 'lastPage'],
+      anchors: ['', 'love-to', 'featured-projects', 'footer'],
       menu: '#menu',
       scrollHorizontally: true,
       scrollOverflow: true,
