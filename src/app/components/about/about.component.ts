86a3489e0c9e78f0f0113ca0f633ed97af8b6a7d
Paula <paula.garcia1@correo.icesi.edu.co>
2020-05-12 20:19:02 -0500

    about started

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import enterView from 'enter-view';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  @ViewChild('hola') one: ElementRef;
+  that = this;
   constructor() { }
 
   ngOnInit() {
+    enterView({
+      selector: '.step',
+      enter(el){
+          el.classList.add('entered');
+      },
+      exit: (el) => {
+        el.classList.remove('entered');
+      },
+      progress: (el, progress) => {
+        el.style.opacity = progress;
+      },
+      offset: 0.5, // enter at middle of viewport
+      once: false, // trigger just once
+    });
   }
 
 }
